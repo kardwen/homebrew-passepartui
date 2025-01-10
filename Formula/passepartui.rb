89b70064fac8ae4b22b93d44aef71324d489dacc
Karl Felix Schewe <info@avoirli.eu>
2025-01-10 17:30:28 +0100

    Update to v0.1.6

--- Formula/passepartui.rb
+++ Formula/passepartui.rb
@@ -1,12 +1,12 @@
 class Passepartui < Formula
   desc "A TUI for pass"
   homepage "https://github.com/kardwen/passepartui"
-  url "https://github.com/kardwen/passepartui/archive/refs/tags/v0.1.5.tar.gz"
-  sha256 "1c1970988576860fdaede17e2287d2a685f6655aaf3ad480625053fc327f1ed5"
+  url "https://github.com/kardwen/passepartui/archive/refs/tags/v0.1.6.tar.gz"
+  sha256 "8e293094b3547565f38b4a5761d210dac83c80fb4403b5c03966a7f5fccee16d"
   license "GPL-3.0-only"
 
   depends_on "rust" => :build
-  depends_on "pass"
+  depends_on "gpgme"
 
   def install
     system "cargo", "install", *std_cargo_args
